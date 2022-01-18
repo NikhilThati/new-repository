ccc182c2228c6bc47ae3e3c3428af1ec6f98413c
NikhilThati <93723309+NikhilThati@users.noreply.github.com>
2022-01-18 20:53:12 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,36 @@
+
+resource "aws_amplify_app" "example" {
+  name       = "example"
+  repository = "https://github.com/NikhilThati/static-website-example.git"
+
+  # The default build_spec added by the Amplify Console for React.
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - yarn install
+        build:
+          commands:
+            - yarn run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  # The default rewrites and redirects added by the Amplify Console.
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+
+  environment_variables = {
+    ENV = "test"
+  }
+}
